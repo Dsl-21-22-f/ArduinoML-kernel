631b096820f7bd3adfda2b1595403a0d59ba74e6
flo <florian.striebel@live.fr>
2022-01-29 14:59:34 +0100

    add or condition

--- embeddeds/groovy/GroovuinoML/scripts/scenar2.groovy
+++ embeddeds/groovy/GroovuinoML/scripts/scenar2.groovy
@@ -8,8 +8,7 @@ state "off" means "buzzer" becomes low and "led" becomes "low"
 
 initial "off"
 
-from off to on when button becomes down
-from off to on when "button2" becomes "down"
+from off to on when button becomes down or "button2" becomes "down"
 from "on" to "off" when "button" becomes "up" and button2 becomes "up"
 
 
--- embeddeds/groovy/GroovuinoML/src/main/groovy/groovuinoml/dsl/GroovuinoMLBasescript.groovy
+++ embeddeds/groovy/GroovuinoML/src/main/groovy/groovuinoml/dsl/GroovuinoMLBasescript.groovy
@@ -1,8 +1,6 @@
 package main.groovy.groovuinoml.dsl
 
 import io.github.mosser.arduinoml.kernel.behavioral.CONDITION
-import io.github.mosser.arduinoml.kernel.behavioral.ConditionType
-import io.github.mosser.arduinoml.kernel.behavioral.OPERATOR
 import io.github.mosser.arduinoml.kernel.behavioral.SensorCondition
 import io.github.mosser.arduinoml.kernel.behavioral.TimeCondition
 import main.groovy.groovuinoml.dsl.GroovuinoMLBinding
@@ -52,44 +50,39 @@ abstract class GroovuinoMLBasescript extends Script {
 	
 	// from state1 to state2 when sensor becomes signal
 	def from(state1) {
+		int nb_Transition = -1
+		List<List<SensorCondition>> sensorConditions = new ArrayList<List<SensorCondition>>();
+		List<TimeCondition> timeConditions = new ArrayList<TimeCondition>()
 		[to: { state2 ->
-			List<SensorCondition> sensorConditions = new ArrayList<SensorCondition>();
-			TimeCondition timeCondition = null
-			((GroovuinoMLBinding) this.getBinding()).getGroovuinoMLModel().createTransition(
-					state1 instanceof String ? (State)((GroovuinoMLBinding)this.getBinding()).getVariable(state1) : (State)state1,
-					state2 instanceof String ? (State)((GroovuinoMLBinding)this.getBinding()).getVariable(state2) : (State)state2,
-					sensorConditions,
-					timeCondition)
+			def newTransition
+			newTransition = {
+				nb_Transition++
+				sensorConditions.add(new ArrayList<SensorCondition>())
+				((GroovuinoMLBinding) this.getBinding()).getGroovuinoMLModel().createTransition(
+						state1 instanceof String ? (State)((GroovuinoMLBinding)this.getBinding()).getVariable(state1) : (State)state1,
+						state2 instanceof String ? (State)((GroovuinoMLBinding)this.getBinding()).getVariable(state2) : (State)state2,
+						sensorConditions[nb_Transition],
+						timeConditions[nb_Transition])
+			}
+			newTransition()
 			def closure
 			closure = { sensor1 ->
 				[becomes: { signal1 ->
 					SensorCondition sensorCond = new SensorCondition();
 					sensorCond.setSensor(sensor1 instanceof String ? (Sensor) ((GroovuinoMLBinding) this.getBinding()).getVariable(sensor1) : (Sensor) sensor1)
 					sensorCond.setValue(signal1 instanceof String ? (CONDITION) ((GroovuinoMLBinding) this.getBinding()).getVariable(signal1) : (CONDITION) signal1)
-					sensorConditions.add(sensorCond)
-					/*[and: { sensor2 ->
-						containsAndOr = true
-						[becomes: { signal2 ->
-							UnaryExpr expr2= new UnaryExpr()
-							expr2.setSensor(
-									sensor1 instanceof String ? (Sensor)((GroovuinoMLBinding)this.getBinding()).getVariable(sensor2) : (Sensor)sensor2)
-							expr2.setValue(
-									signal2 instanceof String ? (SIGNAL)((GroovuinoMLBinding)this.getBinding()).getVariable(signal2) : (SIGNAL)signal2)
-							BinaryExpr binExpr = new BinaryExpr()
-							binExpr.setLeft(expr1)
-							binExpr.setRight(expr2)
-							binExpr.operator(OPERATOR.AND)
-							((GroovuinoMLBinding) this.getBinding()).getGroovuinoMLModel().createTransition(
-									state1 instanceof String ? (State)((GroovuinoMLBinding)this.getBinding()).getVariable(state1) : (State)state1,
-									state2 instanceof String ? (State)((GroovuinoMLBinding)this.getBinding()).getVariable(state2) : (State)state2,
-									binExpr)
-						}]*/
-					[and: closure]
+					sensorConditions[nb_Transition].add(sensorCond)
+					[and: closure,
+					 or : { signal2 ->
+						 newTransition()
+						 closure(signal2)
+					 }]
 				}]
 			}
 			[when: closure,
 			 after : { time ->
-				 timeCondition.setTime(Integer.parseInt(time))
+				 timeConditions.add(new TimeCondition())
+				 timeConditions[nb_Transition].setTime(Integer.parseInt(time))
 				 [and: closure]
 			 }]
 		}]
